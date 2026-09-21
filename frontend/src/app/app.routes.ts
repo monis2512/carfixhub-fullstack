5112ae6b3ee5f6f6c56e0f5e459510c608100079
monis2512 <102420522+monis2512@users.noreply.github.com>
2026-09-21 23:40:54 +0530

    Create app.routes.ts

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '**', redirectTo: '' }
+];
+
