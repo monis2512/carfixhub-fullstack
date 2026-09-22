5c1bf12b3cb74d60fd5147ab9eee43400e275349
monis2512 <102420522+monis2512@users.noreply.github.com>
2026-09-22 23:35:58 +0530

    Create hero-animation.component.ts

---
+++ frontend/src/app/hero-animation/hero-animation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-animation',
+  standalone: true,
+  templateUrl: './hero-animation.component.html',
+  styleUrls: ['./hero-animation.component.css']
+})
+export class HeroAnimationComponent {}
